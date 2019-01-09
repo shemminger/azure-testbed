44136c34fa0ac009a62ae4578e8f8b033846c9ab
Stephen Hemminger <sthemmin@microsoft.com>
2019-01-09 15:46:05 -0800

    preliminary scripts

---
+++ install-dpdk.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+
+# get necessary packages
+apt-get update
+apt-get install git build-essential
+apt-get install libnuma-dev
+
+# get current source
+git clone https://dpdk.org/git/dpdk
+
+cd dpdk
+cat >config/defconfig_azure <<@EOF@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright(c) 2010-2014 Intel Corporation
+
+# subset of x86_64-native-linuxapp-gcc
+
+#include "common_base"
+
+# subset of common_linuxapp
+CONFIG_RTE_EXEC_ENV="linuxapp"
+CONFIG_RTE_EXEC_ENV_LINUXAPP=y
+
+CONFIG_RTE_MACHINE="native"
+
+CONFIG_RTE_ARCH="x86_64"
+CONFIG_RTE_ARCH_X86_64=y
+CONFIG_RTE_ARCH_X86=y
+CONFIG_RTE_ARCH_64=y
+
+CONFIG_RTE_TOOLCHAIN="gcc"
+CONFIG_RTE_TOOLCHAIN_GCC=y
+
+CONFIG_RTE_EAL_NUMA_AWARE_HUGEPAGES=y
+CONFIG_RTE_EAL_IGB_UIO=n
+CONFIG_RTE_EAL_VFIO=n
+CONFIG_RTE_KNI_KMOD=y
+CONFIG_RTE_LIBRTE_KNI=y
+CONFIG_RTE_LIBRTE_PMD_KNI=y
+CONFIG_RTE_LIBRTE_VHOST=y
+CONFIG_RTE_LIBRTE_VHOST_NUMA=y
+CONFIG_RTE_LIBRTE_VHOST_POSTCOPY=n
+CONFIG_RTE_LIBRTE_PMD_VHOST=y
+CONFIG_RTE_LIBRTE_IFC_PMD=y
+CONFIG_RTE_LIBRTE_PMD_AF_PACKET=y
+CONFIG_RTE_LIBRTE_PMD_SOFTNIC=y
+CONFIG_RTE_LIBRTE_PMD_TAP=y
+CONFIG_RTE_PROC_INFO=y
+
+CONFIG_RTE_LIBRTE_VDEV_NETVSC_PMD=y
+CONFIG_RTE_LIBRTE_VMBUS=y
+CONFIG_RTE_LIBRTE_NETVSC_PMD=y
+CONFIG_RTE_LIBRTE_MLX5_PMD=y
+@EOF@
+
+make config T=azure
+make -j8
+
