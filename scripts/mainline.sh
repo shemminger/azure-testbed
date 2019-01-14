96ff5491b1cbf2b30e358517259252c1c47f5817
Stephen Hemminger <sthemmin@microsoft.com>
2019-01-14 15:26:12 -0800

    scripts/mainline: don't wait on reboot

--- scripts/mainline.sh
+++ scripts/mainline.sh
@@ -7,4 +7,4 @@ wget \
     https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.20/linux-modules-4.20.0-042000-generic_4.20.0-042000.201812232030_amd64.deb
 
 sudo dpkg -i linux*.deb
-sudo reboot
+sudo reboot &
