897c7a4ea41a81c4100dc2351defa1d8481b75da
Stephen Hemminger <sthemmin@microsoft.com>
2019-01-14 17:26:57 -0800

    rebind: add error checks
    Make sure device is there!

--- scripts/rebind-uio.sh
+++ scripts/rebind-uio.sh
@@ -9,6 +9,16 @@ if [ ! -d /sys/module/uio_hv_generic ]; then
     echo $NET_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/new_id
 fi
 
+if [ ! -l /sys/class/net/eth1 ]; then
+    echo  "eth1 is missing (already rebound?)"
+    exit 1
+fi
+
 DEV_UUID=$(basename $(readlink /sys/class/net/eth1/device))
+if [ -z "$DEV_UUID" ]; then
+    echo "can't find device UUID"
+    exit 1
+fi
+
 sudo "echo $DEV_UUID > /sys/bus/vmbus/drivers/hv_netvsc/unbind"
 sudo "echo $DEV_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/bind"
