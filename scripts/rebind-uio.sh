bddfc7206ed4a2c9602e6dcb888ca339d1e74a23
Stephen Hemminger <sthemmin@microsoft.com>
2019-01-14 17:13:59 -0800

    rebind: need to be root

--- scripts/rebind-uio.sh
+++ scripts/rebind-uio.sh
@@ -5,11 +5,10 @@
 # Setup UIO
 NET_UUID="f8615163-df3e-46c5-913f-f2d2f965ed0e"
 if [ ! -d /sys/module/uio_hv_generic ]; then
-    modprobe uio_hv_generic
+    sudo modprobe uio_hv_generic
     echo $NET_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/new_id
 fi
 
 DEV_UUID=$(basename $(readlink /sys/class/net/eth1/device))
-echo $DEV_UUID > /sys/bus/vmbus/drivers/hv_netvsc/unbind
-echo $DEV_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/bind
-    
+sudo "echo $DEV_UUID > /sys/bus/vmbus/drivers/hv_netvsc/unbind"
+sudo "echo $DEV_UUID > /sys/bus/vmbus/drivers/uio_hv_generic/bind"
