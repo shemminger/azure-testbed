2232168a3a13eab5edfae939df914924a01a07f3
Stephen Hemminger <sthemmin@microsoft.com>
2019-02-07 17:17:33 -0800

    scripts/mainline: update to 4.20.7
    Latest upstream stable kernel.

--- scripts/mainline.sh
+++ scripts/mainline.sh
@@ -2,9 +2,9 @@
 # Install mainline kernels from Ubuntu
 
 wget \
-    https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.20/linux-headers-4.20.0-042000-generic_4.20.0-042000.201812232030_amd64.deb \
-    https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.20/linux-image-unsigned-4.20.0-042000-generic_4.20.0-042000.201812232030_amd64.deb \
-    https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.20/linux-modules-4.20.0-042000-generic_4.20.0-042000.201812232030_amd64.deb
+    https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.20.7/linux-headers-4.20.7-042007_4.20.7-042007.201902061234_all.deb \
+    https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.20.7/linux-image-unsigned-4.20.7-042007-generic_4.20.7-042007.201902061234_amd64.deb \
+    https://kernel.ubuntu.com/~kernel-ppa/mainline/v4.20.7/linux-modules-4.20.7-042007-generic_4.20.7-042007.201902061234_amd64.deb
 
 sudo dpkg -i linux*.deb
 sudo reboot &
