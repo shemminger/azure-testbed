fe7eedafedc552c792fbdfcb3d7203cda48208c7
Stephen Hemminger <sthemmin@microsoft.com>
2019-03-11 14:25:58 -0700

    vpp: add necessary packages
    Allow installing VPP without DPDK (which gets its own packages).

--- scripts/install-vpp.sh
+++ scripts/install-vpp.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
 
+# get necessary packages
+sudo apt-get install git build-essential pkg-config \
+     libnuma-dev libmnl-dev rdma-core libibverbs-dev -y
+
 cd /usr/src
 git clone https://gerrit.fd.io/r/vpp
 
